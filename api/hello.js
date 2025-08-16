d21c58a3d617b2d1222bf99de6d8451ce94c9e7a
naella <157705599+naellaz@users.noreply.github.com>
2025-08-16 12:30:16 +0700

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,8 +1,8 @@
 export default function handler(req, res) {
   res.status(200).json({
     message: "Hello from Vercel instance!",
-    instaneId: process.env.VERCEL_REGION || "unknown",
-    time: new date().toISOString(),
+    instanceId: process.env.VERCEL_REGION || "unknown",
+    time: new Date().toISOString(),
     memory: process.memoryUsage(),
     uptime: process.uptime()
   });
